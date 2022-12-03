a67489afacb4386274b4cc63d92cce5511b25c96
SARB <mrsinghbusiness05@gmail.com>
2022-12-03 07:05:37 +0530

    First page Done

--- tesla-clone-yt/src/App.js
+++ tesla-clone-yt/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import { BrowserRouter  as Router ,Route ,Routes} from 'react-router-dom'; 
 import Menu from './Menu';
+import Headerblock from './Headerblock';
 
 function App() {
 
@@ -12,7 +13,8 @@ function App() {
     <Router>
          <div className="App">
          <Header ismenuopen = {ismenuopen}  setismenuopen = {setismenuopen} />
-         {ismenuopen && <Menu /> }
+         {ismenuopen && <Menu /> } 
+         <Headerblock />
     </div>
     </Router>
     
---
+++ tesla-clone-yt/src/Headerblock.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './Headerblock.css';
+
+const Headerblock = () => {
+  return (
+    <div className='headerblock'> 
+         <div className="headerblock_info">
+             <div className="headerblock_infotext">
+                <span id = "model-3-name"> Model 3 </span>
+                <Link className='drive-link'> Schedule a Test Drive </Link>
+             </div>
+             <div className="headerblock_actions">
+                 <button className = "headerblock_buttonprimary">    Custom Order  </button>
+                 <button className = "headerblock_buttonsecondary">  Existing Inventory  </button>
+             </div>
+         </div>
+    </div>
+  )
+}
+
+export default Headerblock
